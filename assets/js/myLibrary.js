5d1657fd42b3b51ed8696e0bf12c6faebd13a4ff
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2021-01-12 06:26:48 -0800

    error-2

--- assets/js/myLibrary.js
+++ assets/js/myLibrary.js
@@ -4,6 +4,15 @@ _ = {
             fn(array[i], i, array)
         }
     },
+    filter: (array, fn) => {
+        const filteredData = [];
+        for(let i = 0; i < array.length; i++) {
+            if(fn(array[i], i, array)) {
+                filteredData.push(array[i]);
+            }
+        }
+        return filteredData;
+    },
     
 }
 
@@ -11,4 +20,8 @@ const numbers = [5,2, 22, 3, 41, 1 , 33];
 
 //          ------------------- ForEach
  _.each(numbers, (number) => console.log(number))
+
+ //          ------------------- Filter
+const biggerThan20 = _.filter(numbers, (number) => number > 20);
+console.log(biggerThan20);
 
